2e5e8ea7add4c5b5788007b9d18a4f1479506b33
Yanboww <wywkaixin10@gmail.com>
2024-03-06 21:53:39 -0500

    Added solveQueens and other related methods to test possible combinations and add them into the combinations Arraylist

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -3,21 +3,19 @@ import java.util.Arrays;
 public class ChessBoard {
     private String[][] board;
     final private int n;
+    private ArrayList<String[][]> combinations;
     public ChessBoard(int n)
     {
         this.n = n;
         createBoard();
-        printBoard();
+        combinations = new ArrayList<>();
     }
     public void createBoard()
     {
         board = new String[n][n];
-        for(int i = 0; i < board.length; i++)
+        for(String[] rows : board)
         {
-            for(int a = 0; a < board[i].length; a++)
-            {
-                board[i][a]="-";
-            }
+            Arrays.fill(rows,"-");
         }
     }
     public void printBoard()
@@ -26,6 +24,125 @@ public class ChessBoard {
         {
             System.out.println(Arrays.toString(row));
         }
+        System.out.println();
     }
+
+    public ArrayList<String[][]> solveQueens()
+    {
+        solveQueensCompute(0,n);
+        combinations = checkSolutions();
+        return combinations;
+    }
+    private boolean isValid(int row, int column)
+    {
+        int rowCheck = row;
+        int columnCheck = column;
+        //System.out.println(board[row].length + "------");
+        //System.out.println(board.length);
+        //System.out.println(row);
+        //System.out.println(column);
+        for(int i = 0; i < board.length; i++)
+        {
+            if(board[i][column].equals("Q")) return false;
+        }
+        for(int i = 0; i < board[row].length; i++)
+        {
+            if(board[row][i].equals("Q")) return false;
+        }
+        while(rowCheck>=0 && columnCheck>=0)
+        {
+            if(board[rowCheck][columnCheck].equals("Q")) return false;
+            rowCheck--; columnCheck--;
+        }
+        rowCheck = row;
+        columnCheck = column;
+        while(rowCheck>=0 && columnCheck<board[0].length)
+        {
+            if(board[rowCheck][columnCheck].equals("Q")) return false;
+            rowCheck--; columnCheck++;
+        }
+        rowCheck = row;
+        columnCheck = column;
+        while(rowCheck<board.length && columnCheck<board[0].length)
+        {
+            if(board[rowCheck][columnCheck].equals("Q")) return false;
+            rowCheck++; columnCheck++;
+        }
+        rowCheck = row;
+        columnCheck = column;
+        while(rowCheck<board.length && columnCheck>=0)
+        {
+            if(board[rowCheck][columnCheck].equals("Q")) return false;
+            rowCheck++; columnCheck--;
+        }
+        return true;
+    }
+
+     /* starts at board[0][0] place queen at coord, mark it as "Q"
+        and go recurse the function with the next column and start from row 0 until a tile which "Q" can be placed
+        this repeats until a queen is placed on the last column (adds the board) or if no
+        possible tile is found in which case the function will simply stop the for iteration and end, backtracking
+        to the previous column and reset the coord which the queen was at to "-" before increasing the row until
+        another possible tile is found. This function will completely come to a stop once all tiles of the first column
+        or board[i][0] and its resulting combinations are tested **/
+
+    private void solveQueensCompute(int column, int queens)
+    {
+        //System.out.println(row);
+        //System.out.println(column);
+        //System.out.println(queens);
+        if(queens == 0 || column == board.length) {
+            addBoard();
+            return;
+        }
+        for(int i = 0; i < board.length; i++)
+        {
+            if(isValid(i,column))
+            {
+                board[i][column] = "Q";
+                solveQueensCompute(column+1,queens-1);
+                board[i][column] = "-";
+            }
+        }
+    }
+
+    private void addBoard()
+    {
+        String[][] tempBoard = new String[n][n];
+        for(int i = 0; i < n; i++)
+        {
+            for(int a = 0; a < n; a++)
+            {
+                tempBoard[i][a] = board[i][a];
+            }
+        }
+        combinations.add(tempBoard);
+        //createBoard();
+    }
+    private boolean enoughQueens(String[][] solution)
+    {
+        int count = 0;
+        for(String[] row : solution)
+        {
+            for(String coord : row)
+            {
+                if(coord.equals("Q")) count++;
+            }
+        }
+        if(count == n) return true;
+        return false;
+    }
+
+    private ArrayList<String[][]> checkSolutions()
+    {
+        ArrayList<String[][]> newSolutions = new ArrayList<>();
+        for(String[][] solution : combinations)
+        {
+            if(enoughQueens(solution)) newSolutions.add(solution);
+        }
+        return newSolutions;
+    }
+
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,44 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Please enter a n value(can't be 2,3 or greater than 8: ");
-        int response;
-
+        int response = captureResponse(s);
         ChessBoard c = new ChessBoard(response);
+        ArrayList<String[][]> solved = c.solveQueens();
+        printSolutions(solved);
+        System.out.println("Solutions: " + solved.size());
     }
 
     public static int captureResponse(Scanner s)
     {
+        System.out.print("Please enter a n value(can't be 2,3 or greater than 8): ");
+        int response;
         try{
             response = Integer.parseInt(s.nextLine());
             if(response>8 || response == 2 || response == 3) throw new NumberFormatException();
         }
         catch (NumberFormatException e)
         {
-            System.out.println("Please enter a valid number");
-            System.out.print("Please enter a n value: ");
+            System.out.println("Did not enter a valid n value!\n");
+            response = captureResponse(s);
+        }
+        return response;
+    }
+
+    public static void printSolutions(ArrayList<String[][]> solutions)
+    {
+        for(int i = 0; i < solutions.size(); i++)
+        {
+            String[][] solution = solutions.get(i);
+            for(String[] row : solution)
+            {
+                System.out.println(Arrays.toString(row));
+            }
+            System.out.println();
         }
     }
+
 
 }
